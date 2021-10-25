9fd337f46bd4b3c9a2ec0495eb14088771652e70
Link1515 <h26281660@gmail.com>
2021-10-25 15:26:19 +0800

    飛機遊戲 1.1.1 - 加入計分、修正hover問題

--- JS/05.打殭屍/js/airplaneGame.js
+++ JS/05.打殭屍/js/airplaneGame.js
@@ -1,5 +1,8 @@
 let count = 0;
 let clock = 60;
+let mouseX = 0;
+let mouseY = 0;
+let fireTimer = 0;
 
 $('#startBtn').on('click', gameStart);
 
@@ -22,46 +25,66 @@ function rand(num) {
   return Math.round(Math.random() * num);
 }
 
+// 子彈發射函數
+function trigger() {
+  fireTimer = setInterval(() => {
+    $(`<div class="bullet" style="left: ${mouseX - 15}px; top: ${mouseY - 80}px"></div>`)
+      .appendTo('.box')
+      .animate(
+        {
+          top: '-=1000px',
+        },
+        {
+          specialEasing: {
+            top: 'linear',
+          },
+        }
+      );
+  }, 500);
+}
+
 // 遊戲開始
 function gameStart() {
-  let fireTimer = 0;
-  let mouseX = 0;
-  let mouseY = 0;
+  $('#startBtn').css('display', 'none');
+
+  // reset
+  clock = 10;
+  count = 0;
+  $('#score').text(count);
+
   let blockId = 0;
 
   // block產生
   const blockTimer = setInterval(function () {
-    $(
-      `<div class="block" id="block${blockId}" style="left: ${rand($('.box').width() - 50)}px; top: ${rand(
-        250
-      )}px"></div>`
-    ).appendTo('.box');
-    moveBlock(blockId);
-    blockId++;
-  }, 500);
+    if ($('.block').length < 10) {
+      for (let i = 0; i < rand(3); i++) {
+        $(
+          `<div class="block" id="block${blockId}" style="left: ${rand($('.box').width() - 50)}px; top: ${rand(
+            250
+          )}px"></div>`
+        ).appendTo('.box');
+        moveBlock(blockId);
+        blockId++;
+      }
+    }
+  }, 800);
 
   // 滑鼠控制飛機砲彈
   $('.box').on('mousemove', function (e) {
     mouseX = e.offsetX;
-    mouseY = e.offsetY;
+    if (e.target.classList.contains('box')) {
+      mouseY = e.offsetY;
+    } else {
+      mouseY = e.offsetY + 352;
+    }
+    // mouseY = e.offsetY + 355;
+
+    console.log(e.target);
   });
 
-  $('.box').on('mouseenter', function () {
-    fireTimer = setInterval(() => {
-      $(`<div class="bullet" style="left: ${mouseX - 15}px; top: ${mouseY - 80}px"></div>`)
-        .appendTo('.box')
-        .animate(
-          {
-            top: '-=1000',
-          },
-          {
-            specialEasing: {
-              top: 'linear',
-            },
-          }
-        );
-    }, 500);
-  });
+  trigger();
+
+  $('.box').on('mouseenter', trigger);
 
   $('.box').on('mouseleave', function () {
     clearInterval(fireTimer);
@@ -81,12 +104,14 @@ function gameStart() {
           let _block = $(this);
 
           if (
-            bulletTop < blockTop &&
+            bulletTop < blockTop + _block.height() &&
             bulletLeft + _block.width() > blockLeft &&
             bulletLeft < blockLeft + _block.width()
           ) {
             _block.remove();
             _bullet.remove();
+            count++;
+            $('#score').text(count);
           }
         });
         if (_bullet.offset().top < 0) {
@@ -95,4 +120,18 @@ function gameStart() {
       });
     }
   }, 10);
+
+  const timer = setInterval(function () {
+    clock--;
+    // console.log(clock);
+    if (clock === 0) {
+      clearInterval(timer);
+      clearInterval(deterTimer);
+      clearInterval(blockTimer);
+      clearInterval(fireTimer);
+      $('.box').off();
+      $('.block').remove();
+      $('#startBtn').css('display', 'block');
+    }
+  }, 1000);
 }
