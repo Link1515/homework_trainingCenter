9584bbf447da0f744da40dcbcf78a7b275db9947
Link1515 <h26281660@gmail.com>
2021-10-27 20:42:33 +0800

    flagGame 1.1.0 - 新增開場與過場、解決判斷bug

--- JS/06.翻牌/js/flagGame.js
+++ JS/06.翻牌/js/flagGame.js
@@ -1,34 +1,78 @@
+let stage = 0;
+let cardLength = 0;
+let isMatching = false;
 const countryNameData = ['台灣<br>Taiwan', '美國<br>America', '加拿大<br>Canada'];
 
-for (let i = 0; i < 3; i++) {
-  // 上方旗幟卡片生成
-  $('.upper-box').append(`
-    <div class="flag" data-cardID="${i}">
-      <div class="back"></div>
-      <div class="front"></div>
-    </div>
-  `);
-  $('.flag .front').eq(i).css('background-image', `url(./images/1_${i}.png)`);
+$('#startBtn').on('click', function () {
+  $('#info').hide();
+  $('#game').show();
 
-  // 下方國名卡片生成
-  $('.lower-box').append(`
-    <div class="countryName" data-cardID="${i}">
-      <div class="back"></div>
-      <div class="front">${countryNameData[i]}</div>
-    </div>
-  `);
-}
+  cardCreate(stage);
+});
 
-// 洗牌
-for (let i = 0; i < 3; i++) {
-  const randNum = Math.round(Math.random() * 2);
-  $('.flag').eq(i).insertAfter($('.flag').eq(randNum));
-  $('.countryName').eq(randNum).insertAfter($('.countryName').eq(i));
+// 產生卡片 & 綁定事件
+function cardCreate(stage) {
+  // 等級難度
+  if (stage < 1) {
+    cardLength = 3;
+  } else {
+    cardLength = 6;
+  }
+
+  for (let i = 0; i < cardLength; i++) {
+    // 上方旗幟卡片生成
+    $('.upper-box').append(`
+      <div class="flag" data-cardID="${i}">
+        <div class="back"></div>
+        <div class="front"></div>
+      </div>
+    `);
+    $('.flag .front').eq(i).css('background-image', `url(./images/${stage}_${i}.png)`);
+
+    // 下方國名卡片生成
+    $('.lower-box').append(`
+      <div class="countryName" data-cardID="${i}">
+        <div class="back"></div>
+        <div class="front">${countryNameData[i]}</div>
+      </div>
+    `);
+  }
+
+  // 洗牌
+  for (let i = 0; i < cardLength; i++) {
+    const randNum = Math.round(Math.random() * 2);
+    $('.flag').eq(i).insertAfter($('.flag').eq(randNum));
+    $('.countryName').eq(randNum).insertAfter($('.countryName').eq(i));
+  }
+
+  // 卡片點擊事件
+  $('.flag').on('click', function () {
+    if (!$('.flag').hasClass('open-flag')) {
+      $(this).addClass('open-flag');
+    }
+
+    if (!isMatching) {
+      cardMatch();
+    }
+  });
+
+  $('.countryName').on('click', function () {
+    if (!$('.countryName').hasClass('open-countryName')) {
+      $(this).addClass('open-countryName');
+    }
+
+    if (!isMatching) {
+      cardMatch();
+    }
+  });
 }
 
 // 卡片配對判斷
 function cardMatch() {
   if ($('.open-flag').length && $('.open-countryName').length) {
+    // 正在配對
+    isMatching = true;
+
     if ($('.open-flag').attr('data-cardID') === $('.open-countryName').attr('data-cardID')) {
       $('.open-flag').addClass('OK').off();
       $('.open-countryName').addClass('OK').off();
@@ -39,7 +83,7 @@ function cardMatch() {
       $('#solved').append(`
       <div class="solved-card">
         <div class="front">
-          <div style="background-image: url(./images/1_${$('.open-flag').attr('data-cardID')}.png)"></div>
+          <div style="background-image: url(./images/${stage}_${$('.open-flag').attr('data-cardID')}.png)"></div>
           <p>${$('.open-countryName .front').html()}</p>
         </div>
         <div class="back"></div>
@@ -48,27 +92,18 @@ function cardMatch() {
       setTimeout(function () {
         $('.solved-card').addClass('turnFront');
         $('.solved-card .front').fadeTo(1000, 1);
-      });
+      }, 0);
     }
     setTimeout(function () {
       $('.flag').removeClass('open-flag');
       $('.countryName').removeClass('open-countryName');
+      isMatching = false;
+
+      // 判斷是否過關
+      if ($('.turnFront').length === cardLength) {
+        // stage++;
+        $('.nextStageInfo').show();
+      }
     }, 1000);
   }
 }
-
-$('.flag').on('click', function () {
-  if (!$('.flag').hasClass('open-flag')) {
-    $(this).addClass('open-flag');
-  }
-
-  cardMatch();
-});
-
-$('.countryName').on('click', function () {
-  if (!$('.countryName').hasClass('open-countryName')) {
-    $(this).addClass('open-countryName');
-  }
-
-  cardMatch();
-});
