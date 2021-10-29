c55d521d9710118f940d63d38ec5276bd18d2f4b
Link1515 <h26281660@gmail.com>
2021-10-29 16:24:07 +0800

    flagGame 1.2

--- JS/06.翻牌/js/flagGame.js
+++ JS/06.翻牌/js/flagGame.js
@@ -1,14 +1,91 @@
 let stage = 0;
 let cardLength = 0;
 let isMatching = false;
-const countryNameData = ['台灣<br>Taiwan', '美國<br>America', '加拿大<br>Canada'];
+// prettier-ignore
+const countryNameData = [
+  [
+    '台灣<br>Taiwan', 
+    '美國<br>America', 
+    '加拿大<br>Canada'
+  ],
+  [
+    '瑞典<br>Sweden', 
+    '芬蘭<br>Finland', 
+    '挪威<br>Norway', 
+    '冰島<br>Iceland', 
+    '丹麥<br>Denmark', 
+    '瑞士<br>Switzerland'
+  ],
+  [
+    '比利時<br>Belgium',
+    '德國<br>Germany',
+    '荷蘭<br>Netherlands',
+    '法國<br>France',
+    '義大利<br>Italy',
+    '保加利亞<br>Bulgaria'
+  ],
+  [
+    '馬利<br>Mali',
+    '幾內亞<br>Guinea',
+    '喀麥隆<br>Cameroon',
+    '塞內加爾<br>Senegal',
+    '奈及利亞<br>Nigeria',
+    '查德<br>Chad'
+  ]
+];
 
+// 遊戲開始按鈕
 $('#startBtn').on('click', function () {
-  $('#info').hide();
+  $('#home').hide();
   $('#game').show();
 
+  // 重設下一關按鈕
+  $('#nextBtn').off();
+  nextBtnFun();
+
+  stage = 0;
+
+  gameHtmlReset();
   cardCreate(stage);
 });
+
+// 再次挑戰按鈕
+$('#replayBtn').on('click', function () {
+  stage = 0;
+  $('.finalInfo').hide();
+  $('#game').show();
+
+  // 重設下一關按鈕
+  $('#nextBtn').off();
+  nextBtnFun();
+
+  gameHtmlReset();
+  cardCreate(stage);
+});
+
+// 回首頁按鈕
+$('#homeBtn').on('click', function () {
+  $('.finalInfo').hide();
+  $('#home').show();
+});
+
+function gameHtmlReset() {
+  $('#solved').html('');
+  $('.upper-box').html('');
+  $('.lower-box').html('');
+}
+
+// 下一關按鈕函數
+function nextBtnFun() {
+  $('#nextBtn').on('click', function () {
+    $('.nextStageInfo').hide();
+    $(this).html('進入下一關');
+    stage++;
+
+    gameHtmlReset();
+    cardCreate(stage);
+  });
+}
 
 // 產生卡片 & 綁定事件
 function cardCreate(stage) {
@@ -27,13 +104,13 @@ function cardCreate(stage) {
         <div class="front"></div>
       </div>
     `);
-    $('.flag .front').eq(i).css('background-image', `url(./images/${stage}_${i}.png)`);
+    $('.flag .front').eq(i).css('background-image', `url(./images/${stage}_${i}.gif)`);
 
     // 下方國名卡片生成
     $('.lower-box').append(`
       <div class="countryName" data-cardID="${i}">
         <div class="back"></div>
-        <div class="front">${countryNameData[i]}</div>
+        <div class="front">${countryNameData[stage][i]}</div>
       </div>
     `);
   }
@@ -83,7 +160,7 @@ function cardMatch() {
       $('#solved').append(`
       <div class="solved-card">
         <div class="front">
-          <div style="background-image: url(./images/${stage}_${$('.open-flag').attr('data-cardID')}.png)"></div>
+          <div style="background-image: url(./images/${stage}_${$('.open-flag').attr('data-cardID')}.gif)"></div>
           <p>${$('.open-countryName .front').html()}</p>
         </div>
         <div class="back"></div>
@@ -101,7 +178,16 @@ function cardMatch() {
 
       // 判斷是否過關
       if ($('.turnFront').length === cardLength) {
-        // stage++;
+        // 是否全部過完
+        if (stage === 3) {
+          $('#nextBtn').off();
+          $('#nextBtn').html('挑戰成功!');
+          $('#nextBtn').on('click', function () {
+            $('.nextStageInfo').hide();
+            $('#game').hide();
+            $('.finalInfo').show();
+          });
+        }
         $('.nextStageInfo').show();
       }
     }, 1000);
