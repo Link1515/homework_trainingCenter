a5c0f63045fbc1d4018b0d966076f0be747b18c5
Link1515 <h26281660@gmail.com>
2021-11-10 09:18:12 +0800

    CSS 網頁切版 0.1.3 - banner

--- CSS/08.網頁刻板/js/banner.js
+++ CSS/08.網頁刻板/js/banner.js
@@ -14,6 +14,29 @@ for (let i = 0; i < item.length; i++) {
     url('./images/Banner_BestClass_Blog/${i}.jpg') no-repeat center / cover`;
 }
 
+// main最後插入首個item
+main.insertAdjacentHTML(
+  'beforeend',
+  `
+  <div class="banner_item banner_mainFirst-cloned" style="background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+      url('./images/Banner_BestClass_Blog/0.jpg') no-repeat center / cover">
+        <div>
+          <h1>${item[0].querySelector('h1').innerHTML}</h1>
+          <p>
+            ${item[0].querySelector('p').innerHTML}
+          </p>
+          <button class="primary-btn">
+            <a href="#">Contact Now</a>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+      </div>
+`
+);
+
 // 插入過渡元素
 for (let i = 0; i < item.length; i++) {
   box.insertAdjacentHTML(
@@ -42,8 +65,33 @@ for (let i = 0; i < item.length; i++) {
 // 左右按鈕
 nextBtn.onclick = () => {
   let current = count;
-  count++;
-  count %= item.length;
+
+  countSetting('++');
+  fadeInOut(current);
+};
+
+prevBtn.onclick = () => {
+  let current = count;
+
+  countSetting('--');
+  fadeInOut(current);
+};
+
+function countSetting(str) {
+  if (str === '++') {
+    count++;
+    count %= item.length;
+  } else if (str === '--') {
+    count--;
+    if (count < 0) {
+      count = item.length - 1;
+    }
+  }
+}
+
+function fadeInOut(cur) {
+  // 關閉 transition
+  main.style.transition = '';
 
   // 設置過渡圖片
   if (transitionPic) {
@@ -53,19 +101,35 @@ nextBtn.onclick = () => {
   transitionPic.classList.add('banner_item-cloned-z5');
 
   // 漸淡
-  item[current].style.opacity = '0';
+  item[cur].style.opacity = '0';
 
   setTimeout(() => {
-    item[current].style.opacity = '1';
+    item[cur].style.opacity = '1';
     main.style.left = `-${count * 100}vw`;
   }, 500);
-};
+}
 
-prevBtn.onclick = () => {
-  count--;
-  if (count < 0) {
-    count = item.length - 1;
+// 觸控滑動
+let touchStartPos = 0;
+let touchNewPos = 0;
+
+main.addEventListener('touchstart', (e) => {
+  touchStartPos = e.touches[0].pageX;
+  main.style.transition = '';
+});
+
+main.addEventListener('touchmove', (e) => {
+  touchNewPos = e.touches[0].pageX;
+  main.style.left = `calc(-${count * 100}vw - ${touchStartPos - touchNewPos}px)`;
+});
+
+main.addEventListener('touchend', () => {
+  main.style.transition = 'left 0.5s';
+  if (touchStartPos - touchNewPos > 0) {
+    countSetting('++');
+    main.style.left = `-${count * 100}vw`;
+  } else {
+    countSetting('--');
+    main.style.left = `-${count * 100}vw`;
   }
-
-  main.style.left = `-${count * 100}vw`;
-};
+});
