ed86f20ed4863441c4fab664f800ca078bda1b0c
Link1515 <h26281660@gmail.com>
2021-11-21 11:59:03 +0800

    CSS 網頁切版 0.8.0 - eventCountdown

--- CSS/08.網頁刻板/js/sections/statistics.js
+++ CSS/08.網頁刻板/js/sections/statistics.js
@@ -1,5 +1,13 @@
 const totalTeacher = document.querySelector('.odometer_totalTeacher');
+const schoolBus = document.querySelector('.odometer_schoolBus');
+const ourCourse = document.querySelector('.odometer_ourCourse');
+const awardWinner = document.querySelector('.odometer_awardWinner');
 
-setTimeout(() => {
-  totalTeacher.innerHTML = 1500;
-}, 2000);
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset >= 8360) {
+    totalTeacher.innerHTML = 1500;
+    schoolBus.innerHTML = 4500;
+    ourCourse.innerHTML = 5500;
+    awardWinner.innerHTML = 6500;
+  }
+});
